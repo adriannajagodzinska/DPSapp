5a0e4212cc606ceb909a82079d0b481623befbb9
karolinacicha97 <karolinacicha97@gmail.com>
2021-05-10 17:17:07 +0200

    Dodanie Migracji

---
+++ DPSapp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DPSapp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DPSapp.DAL.DPSContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DPSapp.DAL.DPSContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
