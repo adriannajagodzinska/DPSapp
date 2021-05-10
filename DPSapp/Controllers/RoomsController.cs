118388ef7c288914988ac36c1c2121541c0a2f38
karolinacicha97 <karolinacicha97@gmail.com>
2021-05-10 22:41:31 +0200

    Przeróbki slidera

--- DPSapp/Controllers/RoomsController.cs
+++ DPSapp/Controllers/RoomsController.cs
@@ -1,4 +1,5 @@
-﻿using DPSapp.Models;
+﻿using DPSapp.DAL;
+using DPSapp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,8 @@ namespace DPSapp.Controllers
 {
     public class RoomsController : Controller
     {
-        
 
+        private DPSContext _db = new DPSContext();
 
         // GET: Rooms
         public ActionResult Index()
@@ -25,11 +26,26 @@ namespace DPSapp.Controllers
             
             List<string> list = new List<string>{"~/Images\\1.jpg",
                                             "~/Images\\2.jpg",
-                                            "~/Images\\3.bmp"};
+                                            "~/Images\\3.bmp",
+            "~/Images\\4.jpg"};
             // ""
             ListOfAdresses.ListOfAdresses = list;
 
             return View(ListOfAdresses);
+        }
+
+        public ActionResult ChooseRoomSite()
+        {
+            List<Room> rooms = new List<Room>();
+            Room room1 = new Room();
+            room1.RoomNumber = 1;
+            Room room2 = new Room();
+            room2.RoomNumber = 2;
+            rooms.Add(room1);
+            rooms.Add(room2);
+            //_db.Rooms.Add(room1);
+            //_db.SaveChanges();
+            return View(rooms);
         }
 
     }
