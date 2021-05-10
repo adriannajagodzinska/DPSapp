f8c5d23501d92188730faf44398df43a9161f3e6
karolinacicha97 <karolinacicha97@gmail.com>
2021-05-10 19:17:13 +0200

    Slider Vol1

--- DPSapp/Controllers/RoomsController.cs
+++ DPSapp/Controllers/RoomsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DPSapp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,28 @@ namespace DPSapp.Controllers
 {
     public class RoomsController : Controller
     {
+        
+
+
         // GET: Rooms
         public ActionResult Index()
         {
             return View();
         }
+
+
+        public ActionResult Slider()
+        {
+            ListOfAdressessToMedia ListOfAdresses = new ListOfAdressessToMedia();
+            
+            List<string> list = new List<string>{"~/Images\\1.jpg",
+                                            "~/Images\\2.jpg",
+                                            "~/Images\\3.bmp"};
+            // ""
+            ListOfAdresses.ListOfAdresses = list;
+
+            return View(ListOfAdresses);
+        }
+
     }
 }
---
+++ DPSapp/Models/ListOfAdressessToMedia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DPSapp.Models
+{
+    public class ListOfAdressessToMedia
+    {
+        public List<string> ListOfAdresses { get; set; }
+    }
+}
