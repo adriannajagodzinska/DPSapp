4c16c7fa8c024d2d0c47e959c238c04a61bfa2f5
karolinacicha97 <karolinacicha97@gmail.com>
2021-06-20 18:14:23 +0200

    Wstępna funkcjonalność ładowania plików. Pliki ładowane są do folderu "FilesUpload" w projekcie

---
+++ DPSapp/Controllers/UploadFilesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace DPSapp.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        // GET: UploadFiles
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                if(file.ContentLength>0)
+                {
+                    string filename = Path.GetFileName(file.FileName);
+                    string filepath = Path.Combine(Server.MapPath("~/FilesUpload"),filename);
+                    file.SaveAs(filepath);
+                }
+                ViewBag.Message = "Plik poprawnie załadowany!";
+                return View();
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Plik nie załadował się!";
+                return View();
+            }
+            
+        }
+    }
+}
