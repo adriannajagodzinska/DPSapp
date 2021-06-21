5931fb15a82fb9b37a6cb545b1bac187f4d02147
VetHavelock <58120855+VetHavelock@users.noreply.github.com>
2021-06-21 21:30:38 +0200

    Dodanie wysyłania od rodziny

--- DPSapp/Controllers/FamilyController.cs
+++ DPSapp/Controllers/FamilyController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DPSapp.Models;
+using DPSapp.DAL;
+using System.IO;
 
 namespace DPSapp.Controllers
 {
     public class FamilyController : Controller
     {
         // GET: Family
+        private DPSContext db = new DPSContext();
         public ActionResult Index()
         {
 
@@ -30,5 +34,61 @@ namespace DPSapp.Controllers
             }
             
         }
+
+        public ActionResult Send()
+        {
+            if (Session["role"] != null)
+            {
+                if (Session["role"].ToString() == "2")
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Error401", "Home");
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Error401", "Home");
+            }
+        }
+        [HttpPost]
+        public ActionResult Send([Bind(Include = "Komunikat")] FamilySender fSender, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (file.ContentLength > 0)
+                    {
+                        string filename = Path.GetFileName(file.FileName);
+                        string filepath = Path.Combine(Server.MapPath("~/FilesUpload"), filename);
+                        file.SaveAs(filepath);
+                        string userName = Session["UserName"].ToString();
+                        int pID = db.Users.Where(a => a.Login == userName).Select(a => a.PatientID).FirstOrDefault();
+                        var Tagi = db.Patients.Where(a => a.PatientId == pID).Select(a => a.Tags);
+                        Tag tID = (Tag)Tagi.Select(a => a).FirstOrDefault();
+                        string komunikat = fSender.Komunikat;
+                        string adres = filepath;
+                        Message m = new Message { Image = adres, MessageText = komunikat, Tags = (ICollection<Tag>)tID };
+                        db.Messages.Add(m);
+                        db.SaveChanges();
+                    }
+                    ViewBag.Message = "Plik poprawnie załadowany!";
+                    return View();
+                }
+                catch (Exception)
+                {
+                    ViewBag.Message = "Plik nie załadował się!";
+                    return View();
+                }
+                
+            }
+            return View(fSender);
+        }
+
+        
     }
 }
---
+++ DPSapp/Models/FamilySender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DPSapp.Models
+{
+    public class FamilySender
+    {
+        
+        public string Komunikat;
+        
+    }
+}
