b7ae0ffaaafa3b884ce2f5a243065514d7b7c889
VetHavelock <58120855+VetHavelock@users.noreply.github.com>
2021-06-24 17:43:26 +0200

    Rodzina - dodanie tagu do wiadomości

--- DPSapp/Controllers/FamilyController.cs
+++ DPSapp/Controllers/FamilyController.cs
@@ -63,19 +63,25 @@ namespace DPSapp.Controllers
                 {
                    if (fSender.file.ContentLength > 0)
                     {
+                        //Patient p = db.Patients.Where(a=>a.PatientId==2).Select(a => a).FirstOrDefault();
+                        //p.Tags = new List<Tag>();
+                        //Tag t = db.Tags.Where(a => a.TagId == 1).Select(a => a).FirstOrDefault();
+                        //p.Tags.Add(t);
+                        //db.SaveChanges();
                         string filename = Path.GetFileName(fSender.file.FileName);
                         string filepath = Path.Combine(Server.MapPath("~/FilesUpload"), filename);
                         fSender.file.SaveAs(filepath);
                         string userName = Session["UserName"].ToString();
                         int pID = db.Users.Where(a => a.Login == userName).Select(a => a.PatientID).FirstOrDefault();
-                        //var Tagi = db.Patients.Where(a => a.PatientId == pID).Select(a => a.Tags);
-                        //Tag tID = (Tag)Tagi.Select(a => a).FirstOrDefault();
+                        var pacjent = db.Patients.Where(a => a.PatientId == pID).Select(a => a).FirstOrDefault();
+                        var Tagi = pacjent.Tags;
+                        var tID = Tagi.Select(a => a).FirstOrDefault();
                         
                         string komunikat = fSender.Komunikat;
                         string adres = filepath;
                         Message m = new Message { Image = adres, MessageText = komunikat};
-                        
-                        //m.Tags.Add(tID);
+                        m.Tags = new List<Tag>();
+                        m.Tags.Add(tID);
                         db.Messages.Add(m);
                         db.SaveChanges();
                         //int mID = db.Messages.Where(a => a.Image == adres).Select(a => a.MessageId).FirstOrDefault();
