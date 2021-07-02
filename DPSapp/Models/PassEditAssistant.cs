02155c0bc17160e933e1bc8cb2b1d3e77649df7f
VetHavelock <58120855+VetHavelock@users.noreply.github.com>
2021-07-02 13:42:25 +0200

    Poprawa wyświetlania w zmianie hasła

--- DPSapp/Models/PassEditAssistant.cs
+++ DPSapp/Models/PassEditAssistant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace DPSapp.Models
 {
     public class PassEditAssistant
     {
+        [DisplayName("Stare hasło")]
         public string OldPass { get; set; }
+        [DisplayName("Nowe hasło")]
         public string NewPass1 { get; set; }
+        [DisplayName("Powtórz nowe hasło")]
         public string NewPass2 { get; set; }
     }
 }
