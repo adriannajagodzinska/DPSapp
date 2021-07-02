ec954d112bb42d80c8d0d43982895214eaefedfb
VetHavelock <58120855+VetHavelock@users.noreply.github.com>
2021-07-02 13:03:42 +0200

    Dodana zmiana hasła dla pracowników

---
+++ DPSapp/Models/PassEditAssistant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DPSapp.Models
+{
+    public class PassEditAssistant
+    {
+        public string OldPass { get; set; }
+        public string NewPass1 { get; set; }
+        public string NewPass2 { get; set; }
+    }
+}
